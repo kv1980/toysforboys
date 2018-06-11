55db2a0bdf1378dfb37a9acd6aacd4b087054a5b
Kristof Vermeulen <kristofvermeulen1980@gmail.com>
2018-06-11 08:34:24 +0200

    eerste commit

---
+++ src/main/java/be/vdab/toysforboys/ToysforboysApplication.java
@@ -0,0 +1,12 @@
+package be.vdab.toysforboys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToysforboysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToysforboysApplication.class, args);
+	}
+}
